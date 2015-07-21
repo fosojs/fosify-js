60f5a5251ec4ef10e435e3066b5426d96bc07c72
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-07-21 21:36:34 +0300

    insertGlobalVars covered with unit tests.

---
+++ test/insert-global-vars/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var expect = require('chai').expect;
+var insertGlobalVars = require('../../lib/insert-global-vars');
+
+describe('insertGlobalVars', function() {
+  it('inserts all the required vars', function() {
+    var packageVersion = require('../../package.json').version;
+    var globalVars = insertGlobalVars.create({
+      host: 'example.com',
+      secureHost: 'secure.example.com',
+      baseURL: '/base/url'
+    });
+
+    expect(globalVars.__host()).to.eq('"example.com"');
+    expect(globalVars.__secureHost()).to.eq('"secure.example.com"');
+    expect(globalVars.__baseURL()).to.eq('"/base/url"');
+    expect(globalVars.__version()).to.eq('"' + packageVersion + '"');
+  });
+});
